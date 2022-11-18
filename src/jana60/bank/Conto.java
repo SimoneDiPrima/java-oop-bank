eb84cc01435cc337116044f6a71049b54a7da626
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-18 10:05:33 +0100

    second commit

--- src/jana60/bank/Conto.java
+++ src/jana60/bank/Conto.java
@@ -1,5 +1,43 @@
 package jana60.bank;
 
-public class Conto {
+import java.util.Random;
 
+public class Conto{
+	
+	public String name;
+	private int iban;
+	private int saldo = 0;
+	
+	public Conto(String name,int iban,int saldo) {
+
+		this.name = name;
+		Random rnd = new Random();
+		this.iban = rnd.nextInt(99999);
+		this.saldo = saldo;
+	}
+	public int getIban() {
+		
+		return iban;
+	}
+	public void setIban(int iban) {
+		
+		this.iban = iban;
+	}
+	public int getSaldo() {
+		
+		return saldo;
+	}
+	public void setSaldo(int saldo) {
+		
+		this.saldo = saldo;
+	}
+
+
+
+public String toString() {
+		
+		return "Nome Titolare: " + name
+				+ "\nIban contocorrente: " + iban
+				+ "\nil saldo attuale è: " + saldo;
+	}
 }
--- src/jana60/bank/main.java
+++ src/jana60/bank/main.java
@@ -1,5 +1,31 @@
 package jana60.bank;
 
-public class main {
+import java.util.Scanner;
+import java.util.Random;
 
+
+public class main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("name: ");
+		String name = sc.nextLine();
+		Random rnd = new Random();
+		int iban = rnd.nextInt(99999);
+		int saldo = 0;
+		Conto c = new Conto(name,iban,saldo);
+		System.out.println(c);
+		System.out.println("Quanti soldi vuoi aggiungere??");
+		int addMoney = sc.nextInt();
+		System.out.println("il tuo nuovo saldo sara di :" + addMoney + "€");
+		System.out.println("Quanti soldi vuoi prelevare??");
+		int getMoney = sc.nextInt();
+		saldo = addMoney - getMoney;
+		if(saldo <= 0 ) {
+			System.out.print("siamo spiacenti il tuo saldo non ti consente di ritirare la cifra richiesta");
+		}
+		else {
+			System.out.print("il tuo nuovo saldo sara di: " + saldo + "€");
+		}
+	}
 }
